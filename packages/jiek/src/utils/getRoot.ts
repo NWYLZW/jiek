b6df0040b725336da47606814a2b62c20a49b0ba
yijie4188 <yijie4188@gmail.com>
2024-04-09 16:29:01 +0800

    refactor(jiek): cache root option value

--- packages/jiek/src/utils/getRoot.ts
+++ packages/jiek/src/utils/getRoot.ts
@@ -2,11 +2,15 @@ import path from 'node:path'
 
 import { program } from 'commander'
 
+let root: string
 export function getRoot() {
+  if (root) return root
+
   const rootOption = program.getOptionValue('root')
-  return rootOption
+  root = rootOption
     ? path.isAbsolute(rootOption)
       ? rootOption
       : path.resolve(process.cwd(), rootOption)
     : process.cwd()
+  return root
 }
