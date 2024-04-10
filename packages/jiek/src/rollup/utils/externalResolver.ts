2e4a8f45369c71100cb8216f0e671346c597b981
yijie4188 <yijie4188@gmail.com>
2024-04-10 23:14:46 +0800

    feat(jiek): mark optionsDependencies and `node:` as externals

--- packages/jiek/src/rollup/utils/externalResolver.ts
+++ packages/jiek/src/rollup/utils/externalResolver.ts
@@ -2,10 +2,14 @@ export default function () {
   const cwd = process.cwd()
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const pkg = require(`${cwd}/package.json`)
-  const { dependencies = {}, peerDependencies = {} } = pkg
+  const { dependencies = {}, peerDependencies = {}, optionalDependencies = {} } = pkg
   const external = <(string | RegExp)[]>Object
     .keys(dependencies)
     .concat(Object.keys(peerDependencies))
+    .concat(Object.keys(optionalDependencies))
   return external
     .map(dep => new RegExp(`^${dep}(/.*)?$`))
+    .concat([
+      /^node:/
+    ])
 }
