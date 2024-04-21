b78eca4fa26fa649d320fa3f5f1d4991cf6013ab
yijie4188 <yijie4188@gmail.com>
2024-04-22 01:03:59 +0800

    feat(vite-plugin-glob-accept): add client export

---
+++ packages/vite-plugins/glob-accept/src/client.ts
@@ -0,0 +1,12 @@
+import type { ModuleNamespace } from 'vite/types/hot.d.ts'
+
+declare module 'vite/types/hot.d.ts' {
+  interface ViteHotContext {
+    accept(
+      deps: readonly string[],
+      cb: (mods: Array<ModuleNamespace | undefined>, paths?: string[]) => void
+    ): void
+  }
+}
+
+export {}
