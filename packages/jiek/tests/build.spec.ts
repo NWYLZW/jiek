126037da90d96a12621f33cc57ac64a0b8375f27
yijie4188 <yijie4188@gmail.com>
2024-05-18 17:46:26 +0800

    feat(jiek): snapshot for build command

--- packages/jiek/tests/build.spec.ts
+++ packages/jiek/tests/build.spec.ts
@@ -6,7 +6,7 @@ import path from 'node:path'
 
 import * as childProcess from 'child_process'
 import { program } from 'commander'
-import { afterAll, beforeAll, describe, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 
 import { actionFuture } from '../src/inner'
 
@@ -17,7 +17,7 @@ const getROOT = (paths: string[]) => path.resolve(__dirname, 'fixtures', ...path
 function prepareROOT(paths: string[]) {
   const ROOT = getROOT(paths)
   beforeAll(() => {
-    childProcess.execSync('pnpm i', {
+    childProcess.execSync('pnpm i --no-lock', {
       cwd: ROOT,
       stdio: 'inherit'
     })
@@ -29,10 +29,22 @@ function prepareROOT(paths: string[]) {
 }
 
 describe('base', () => {
-  const [, prefixes] = prepareROOT(['base'])
-  test('common', () => {
+  const [root, prefixes] = prepareROOT(['base'])
+  test('common', async () => {
     process.argv = [...prefixes, '--filter', 'test-foo']
     program.parse(process.argv)
-    return actionFuture
+    await actionFuture
+    const distDir = path.resolve(root, 'packages/foo/dist')
+    // expect the root/foo/dist to be created
+    expect(fs.existsSync(distDir)).toBeTruthy()
+    // snapshot the root/foo/dist files content
+    const files = fs.readdirSync(distDir, { recursive: true })
+    files.forEach((file) => {
+      if (typeof file !== 'string') return
+      expect(`${file}:\n${
+        fs.readFileSync(path.resolve(distDir, file), 'utf-8')
+      }`).toMatchSnapshot()
+    })
+    fs.rmSync(distDir, { recursive: true })
   })
 })
