18ea4a38247ee0bc18ef3af849a8fb28bbaf373e
yijie4188 <yijie4188@gmail.com>
2024-05-23 03:29:47 +0800

    fix(jiek): relative path is bad

--- packages/jiek/src/commands/base.ts
+++ packages/jiek/src/commands/base.ts
@@ -1,7 +1,5 @@
 import { program } from 'commander'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require('../../package.json')
+import { pkg } from '../pkg'
 
 program
   .version(pkg.version)
