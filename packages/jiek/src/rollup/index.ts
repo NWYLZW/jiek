7161a13d388ebff14c9f531b5aac6d3d88808e25
yijie4188 <yijie4188@gmail.com>
2024-05-23 23:20:53 +0800

    fix(jiek): progress finished status compute error

--- packages/jiek/src/rollup/index.ts
+++ packages/jiek/src/rollup/index.ts
@@ -4,11 +4,11 @@ import type { Options } from '@jiek/pkger'
 import { getWorkspaceDir } from '@jiek/utils/getWorkspaceDir'
 import json from '@rollup/plugin-json'
 import autoprefixer from 'autoprefixer'
-import { InputPluginOption, Plugin, RollupOptions } from 'rollup'
+import { MultiBar, Presets } from 'cli-progress'
+import type { InputPluginOption, Plugin, RollupOptions } from 'rollup'
 import { dts } from 'rollup-plugin-dts'
 import esbuild from 'rollup-plugin-esbuild'
 import postcss from 'rollup-plugin-postcss'
-import { MultiBar, Presets } from 'cli-progress'
 
 import { createGlobalsLinkage } from './plugins/globals'
 import skip from './plugins/skip'
@@ -21,10 +21,9 @@ function resolveWorkspacePath(p: string) {
   return resolve(workspaceRoot, p)
 }
 
-function progress(
+function watchProgress(
   entries: string[],
   onProcessing: (entry: string) => void,
-  onTransforming: (id: string) => void,
   onProcessed: (name: string, fileName: string) => void
 ): Plugin {
   return {
@@ -32,9 +31,6 @@ function progress(
     resolveId(source, importer) {
       if (entries.includes(source) || importer === undefined)
         onProcessing(source)
-    },
-    transform(_, id) {
-      onTransforming(id)
     },
     writeBundle(options, bundle) {
       Object.entries(bundle).forEach(([key, value]) => {
@@ -102,6 +98,10 @@ export const template = (
         typeof value === 'string' ? value : value['inner-src']
       ])
   )
+  const maxLengthEntry = Math.max(...Object.values(exportsEntries).map(v => v.length))
+  function paddingEntry(entry: string) {
+    return entry.padEnd(maxLengthEntry)
+  }
 
   const [globalsRegister, globalsOutput] = createGlobalsLinkage()
   const external = externalResolver()
@@ -117,24 +117,54 @@ export const template = (
     format: '- {bar} | {type} | {entry} | {message}'
   }, Presets.shades_classic)
   const entries = Object.values(exportsEntries)
-  const progressState = {
-    esm: entries.reduce((add, entry) => {
+  const addBarsByEntries = (type: 'esm' | 'umd' | 'dts') => entries
+    .reduce((add, entry) => {
       const bar = multiBars.create(100, 0)
-      bar.update(0, { type: 'esm', entry, message: 'waiting' })
-      return { ...add, [entry]: bar }
-    }, {} as Record<string, ReturnType<typeof multiBars.create>>),
-    umd: entries.reduce((add, entry) => {
-      const bar = multiBars.create(100, 0)
-      bar.update(0, { type: 'umd', entry, message: 'waiting' })
-      return { ...add, [entry]: bar }
-    }, {} as Record<string, ReturnType<typeof multiBars.create>>),
-    dts: entries.reduce((add, entry) => {
-      const bar = multiBars.create(100, 0)
-      bar.update(0, { type: 'dts', entry, message: 'waiting' })
+      bar.update(0, { type, entry: paddingEntry(entry), message: 'waiting' })
       return { ...add, [entry]: bar }
     }, {} as Record<string, ReturnType<typeof multiBars.create>>)
+  const progressState = {
+    esm: addBarsByEntries('esm'),
+    umd: addBarsByEntries('umd'),
+    dts: addBarsByEntries('dts')
   }
   const resolved = entries.reduce((entries, entry) => ({ ...entries, [entry]: false }), {} as Record<string, boolean>)
+  const watchingProgress = (
+    type: 'esm' | 'umd' | 'dts',
+    defaultEntry?: string
+  ) => {
+    const group = progressState[type]
+    return watchProgress(
+      Object.values(exportsEntries),
+      entry => {
+        const e = defaultEntry ?? entry
+        const bar = group[e]
+        if (!bar) return
+        bar.update(0, { type, entry: paddingEntry(e), message: 'processing' })
+      },
+      (name, fileName) => {
+        const e = defaultEntry ?? exportsEntries[name]
+        const bar = group[e]
+        if (!bar) return
+        const endsReg = {
+          esm: /\.min\.js$/,
+          umd: /\.min\.c?js$/,
+          dts: /\.esm\.d\.ts$/
+        }[type]
+        if (!endsReg.test(fileName)) {
+          bar.update(95, { type, entry: paddingEntry(e), message: 'processed' })
+        } else {
+          bar.update(100, { type, entry: paddingEntry(e), message: 'finished' })
+        }
+        if (type === 'dts') {
+          resolved[e] = true
+          if (Object.values(resolved).every(Boolean)) {
+            setTimeout(() => multiBars.stop(), 200)
+          }
+        }
+      }
+    )
+  }
   return [
     {
       ...commonOptions,
@@ -148,29 +178,7 @@ export const template = (
         })
       ],
       plugins: [
-        progress(
-          Object.values(exportsEntries),
-          entry => {
-            const bar = progressState.esm[entry]
-            if (!bar) return
-            bar.update(0, { type: 'esm', entry, message: 'processing' })
-          },
-          id => {
-            const bar = progressState.esm[id]
-            if (!bar) return
-            bar.update(50, { type: 'esm', entry: id, message: `transforming ${id}` })
-          },
-          (name, fileName) => {
-            const entry = exportsEntries[name]
-            const bar = progressState.esm[entry]
-            if (!bar) return
-            if (!fileName.endsWith('.min.js')) {
-              bar.update(95, { type: 'esm', entry, message: `processed` })
-            } else {
-              bar.update(100, { type: 'esm', entry, message: `finished` })
-            }
-          }
-        ),
+        watchingProgress('esm'),
         commonPlugins,
         globalsRegister({ external }),
         styled && skip({ patterns: [/\.s?css$/] }),
@@ -209,28 +217,7 @@ export const template = (
           })
         ],
         plugins: [
-          progress(
-            [input],
-            entry => {
-              const bar = progressState.umd[entry]
-              if (!bar) return
-              bar.update(0, { type: 'umd', entry, message: 'processing' })
-            },
-            id => {
-              const bar = progressState.umd[input]
-              if (!bar) return
-              bar.update(50, { type: 'umd', input, message: `transforming ${id}` })
-            },
-            (name, fileName) => {
-              const bar = progressState.umd[input]
-              if (!bar) return
-              if (!fileName.endsWith('.min.js')) {
-                bar.update(95, { type: 'umd', input, message: `processed` })
-              } else {
-                bar.update(100, { type: 'umd', input, message: `finished` })
-              }
-            }
-          ),
+          watchingProgress('umd', input),
           commonPlugins,
           globalsOutput,
           styled && postcss({
@@ -259,35 +246,7 @@ export const template = (
         }
       ],
       plugins: [
-        progress(
-          Object.values(exportsEntries),
-          entry => {
-            const bar = progressState.dts[entry]
-            if (!bar) return
-            bar.update(0, { type: 'dts', entry, message: 'processing' })
-          },
-          id => {
-            const bar = progressState.dts[id]
-            if (!bar) return
-            bar.update(50, { type: 'dts', entry: id, message: `transforming ${id}` })
-          },
-          (name, fileName) => {
-            const entry = exportsEntries[name]
-            const bar = progressState.dts[entry]
-            if (!bar) return
-            if (!fileName.endsWith('.esm.d.ts')) {
-              bar.update(95, { type: 'dts', entry, message: `processed` })
-            } else {
-              bar.update(100, { type: 'dts', entry, message: `finished` })
-              resolved[entry] = true
-            }
-            if (Object.values(resolved).every(Boolean)) {
-              setTimeout(() => {
-                multiBars.stop()
-              }, 100)
-            }
-          }
-        ),
+        watchingProgress('dts'),
         commonPlugins,
         styled && skip({ patterns: [/\.s?css$/] }),
         dts({ tsconfig: resolveWorkspacePath('tsconfig.dts.json') }),
