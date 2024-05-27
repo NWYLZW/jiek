d9e1cbbcfd521ed86a01c81bcf6eb48bb76ed9c8
yijie4188 <yijie4188@gmail.com>
2024-05-27 21:33:16 +0800

    chore(jiek): use prepareROOT

--- packages/jiek/tests/publish.spec.ts
+++ packages/jiek/tests/publish.spec.ts
@@ -1,31 +1,16 @@
 import '../src/commands/base'
 import '../src/commands/publish'
 
-import fs from 'node:fs'
-import path from 'node:path'
-
-import * as childProcess from 'child_process'
 import { program } from 'commander'
-import { afterAll, beforeAll, describe, test } from 'vitest'
+import { describe, test } from 'vitest'
 
 import { actionFuture } from '../src/inner'
+import { prepareROOT } from './prepareROOT'
 
-const ROOT = path.resolve(__dirname, 'fixtures/base')
-
-beforeAll(() => {
-  childProcess.execSync('pnpm i', {
-    cwd: ROOT,
-    stdio: 'inherit'
-  })
-})
-afterAll(() => {
-  fs.rmSync(path.resolve(ROOT, 'node_modules'), { recursive: true })
-})
-
-const prefixes = ['node', 'jiek', '--root', ROOT, 'publish']
 describe('publish', () => {
+  const [, prefixes] = prepareROOT(['base'])
   test('base', () => {
-    process.argv = [...prefixes, '--filter', 'test-foo']
+    process.argv = [...prefixes, '--filter', 'test-foo', '-p']
     program.parse(process.argv)
     return actionFuture
   })
