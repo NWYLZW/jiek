fa8670e4c33c7fc99bf5f3833d28953cd1767c30
yijie4188 <yijie4188@gmail.com>
2024-05-28 00:01:59 +0800

    chore(jiek): more test case

--- packages/jiek/tests/publish.spec.ts
+++ packages/jiek/tests/publish.spec.ts
@@ -9,10 +9,21 @@ import { prepareROOT } from './prepareROOT'
 
 const prepareRootWithSubCmd = prepareROOT.bind(null, 'publish')
 
-describe('publish', () => {
+describe('base', () => {
   const [, prefixes] = prepareRootWithSubCmd(['base'])
-  test('base', () => {
+  test('common', () => {
     process.argv = [...prefixes, '--filter', 'test-foo', '-p']
+    program.parse(process.argv)
+    return actionFuture
+  })
+})
+
+describe('single package and multiple entries', () => {
+  const [, prefixes] = prepareRootWithSubCmd(['single-package-and-multiple-entries'], {
+    notWorkspace: true
+  })
+  test('common', () => {
+    process.argv = [...prefixes, '-p']
     program.parse(process.argv)
     return actionFuture
   })
