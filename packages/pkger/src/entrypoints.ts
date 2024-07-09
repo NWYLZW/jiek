7c6f019cdbd49f7028a7e193986181876f8844c0
yijie4188 <yijie4188@gmail.com>
2024-07-09 21:14:31 +0800

    feat: entrypoints to `exports` algorithm

---
+++ packages/pkger/src/entrypoints.ts
@@ -0,0 +1,89 @@
+import path from 'node:path'
+
+import { commondir } from '@jiek/utils/commondir'
+
+export interface Entrypoints2ExportsOptions {
+  /**
+   * @default 'dist'
+   */
+  outdir?: string
+  /**
+   * @default `process.cwd()`
+   */
+  cwd?: string
+}
+
+type RecursiveRecord<T> = {
+  [K in string]: T | RecursiveRecord<T>
+}
+
+function getAllLeafs(obj: RecursiveRecord<string>): string[] {
+  return Object
+    .entries(obj)
+    .reduce<string[]>((acc, [, value]) => {
+      if (typeof value === 'string') {
+        acc.push(value)
+      } else {
+        acc.push(...getAllLeafs(value))
+      }
+      return acc
+    }, [])
+}
+
+export function entrypoints2Exports(
+  entrypoints: string | string[] | Record<string, unknown>,
+  options: Entrypoints2ExportsOptions = {}
+): Record<string, unknown> {
+  const {
+    outdir = 'dist',
+    cwd = process.cwd()
+  } = options
+  let entrypointMapping: Record<string, unknown> = {}
+  let dir: string | undefined
+  if (typeof entrypoints === 'string') {
+    entrypointMapping = { '.': entrypoints }
+    dir = path.dirname(entrypoints)
+  }
+  if (Array.isArray(entrypoints)) {
+    dir = entrypoints.length > 1
+      ? commondir(entrypoints, cwd)
+        .replace(`${cwd}/`, '')
+        .replace(/\/$/, '')
+      : path.dirname(entrypoints[0])
+    entrypoints.forEach((point, i) => {
+      const trimmedCommonDirPath = point
+        .replace(`${dir!}/`, '')
+      const isIndex = i === 0 && trimmedCommonDirPath.match(/index\.[c|m]?[t|j]sx?$/)?.length
+      if (isIndex) {
+        entrypointMapping['.'] = point
+      } else {
+        entrypointMapping[`./${
+          trimmedCommonDirPath
+            .replace(/\.([c|m])?[t|j]sx?$/, '')
+        }`] = point
+      }
+    })
+  } else {
+    if (typeof entrypoints === 'object') {
+      entrypointMapping = entrypoints
+      const leafs = getAllLeafs(entrypoints as RecursiveRecord<string>)
+      dir = leafs.length > 1
+        ? commondir(leafs, cwd)
+          .replace(`${cwd}/`, '')
+          .replace(/\/$/, '')
+        : path.dirname(leafs[0])
+    }
+  }
+  Object
+    .entries(entrypointMapping)
+    .forEach(([key, value]) => {
+      let newValue = value
+      if (typeof value === 'string') {
+        newValue = value
+          .replace(dir!, outdir)
+          .replace(/\.([c|m])?[t|j]sx?$/, '.$1js')
+      }
+      entrypointMapping[key] = newValue
+    })
+  return entrypointMapping
+}
---
+++ packages/pkger/tests/entrypoints.spec.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test } from 'vitest'
+
+import { entrypoints2Exports } from '../src/entrypoints'
+
+describe('entrypoints2Exports', () => {
+  test('single file', () => {
+    expect(entrypoints2Exports('src/index.ts')).toStrictEqual({ '.': 'dist/index.js' })
+    expect(entrypoints2Exports('src/foo.ts')).toStrictEqual({ '.': 'dist/foo.js' })
+  })
+  test('multiple files array', () => {
+    expect(entrypoints2Exports([
+      'src/index.ts'])).toStrictEqual({
+      '.': 'dist/index.js'
+    })
+    expect(entrypoints2Exports([
+      'src/index.ts', 'src/foo.ts'])).toStrictEqual({
+      '.': 'dist/index.js',
+      './foo': 'dist/foo.js'
+    })
+  })
+  test('multiple files object', () => {
+    expect(entrypoints2Exports({
+      '.': 'src/index.ts'
+    })).toStrictEqual({
+      '.': 'dist/index.js'
+    })
+    expect(entrypoints2Exports({
+      '.': 'src/index.ts'
+    })).toStrictEqual({
+      '.': 'dist/index.js'
+    })
+    expect(entrypoints2Exports({
+      './foo': 'src/foo.ts'
+    })).toStrictEqual({
+      './foo': 'dist/foo.js'
+    })
+    expect(entrypoints2Exports({
+      '.': 'src/index.ts',
+      './foo': 'src/foo.ts'
+    })).toStrictEqual({
+      '.': 'dist/index.js',
+      './foo': 'dist/foo.js'
+    })
+  })
+})
