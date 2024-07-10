222962261f656ede6b53df6e2f9bb504645eafcd
yijie4188 <yijie4188@gmail.com>
2024-07-10 20:45:50 +0800

    style(pkger): code style

--- packages/pkger/tests/entrypoints.spec.ts
+++ packages/pkger/tests/entrypoints.spec.ts
@@ -4,8 +4,12 @@ import { entrypoints2Exports } from '../src/entrypoints'
 
 describe('entrypoints2Exports', () => {
   test('single file', () => {
-    expect(entrypoints2Exports('src/index.ts')).toStrictEqual({ '.': 'dist/index.js' })
-    expect(entrypoints2Exports('src/foo.ts')).toStrictEqual({ '.': 'dist/foo.js' })
+    expect(entrypoints2Exports('src/index.ts')).toStrictEqual({
+      '.': 'dist/index.js'
+    })
+    expect(entrypoints2Exports('src/foo.ts')).toStrictEqual({
+      '.': 'dist/foo.js'
+    })
   })
   test('multiple files array', () => {
     expect(entrypoints2Exports([
