ed31c0ddf5cf81b2da31f9fcd6b8509ed8adfbab
yijie4188 <yijie4188@gmail.com>
2024-09-09 00:16:56 +0800

    chore(jiek): style dprint

--- packages/jiek/src/rollup/utils/withMinify.ts
+++ packages/jiek/src/rollup/utils/withMinify.ts
@@ -1,15 +1,17 @@
 import terser from '@rollup/plugin-terser'
 import type { OutputOptions, OutputPlugin } from 'rollup'
 
-export default function (output: OutputOptions & {
-  entryFileNames?: string
-  plugins?: OutputPlugin[]
-}): OutputOptions[] {
+export default function(
+  output: OutputOptions & {
+    entryFileNames?: string
+    plugins?: OutputPlugin[]
+  }
+): OutputOptions[] {
   return [
     output,
     {
       ...output,
-      entryFileNames: output.entryFileNames?.replace(/(\.c?js)$/, '.min$1'),
+      entryFileNames: output.entryFileNames?.replace(/(\.[cm]?js)$/, '.min$1'),
       plugins: [...(output.plugins ?? []), terser()]
     }
   ]
