e33709f98cf58c8375b37310b710a5aec87e3545
yijie4188 <yijie4188@gmail.com>
2024-10-01 00:03:14 +0800

    chore(jiek): test

--- packages/jiek/tests/rollup/template.spec.ts
+++ packages/jiek/tests/rollup/template.spec.ts
@@ -13,6 +13,15 @@ describe('rollup - template', () => {
       exports: './src/index.ts'
     })).toMatchSnapshot()
   })
+  test('with package.json export and should skip it', () => {
+    template({
+      name: 'foo',
+      exports: {
+        './package.json': './package.json',
+        '.': './src/index.ts'
+      }
+    }).forEach(c => console.log(c))
+  })
   test('with import or require conditional', () => {
     expect(template({
       name: 'foo',
