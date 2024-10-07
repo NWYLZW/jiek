e8a494d458a83ab3ea53b1916b07c9ea2cec8d04
YiJie <yijie4188@gmail.com>
2024-10-08 00:27:56 +0800

    feat(jiek): compute compiler options

--- packages/jiek/src/rollup/v2.ts
+++ packages/jiek/src/rollup/v2.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs'
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
 
 import type { RecursiveRecord } from '@jiek/pkger/entrypoints'
 import {
@@ -13,10 +13,13 @@ import { dts } from '@jiek/rollup-plugin-dts'
 import { getWorkspaceDir } from '@jiek/utils/getWorkspaceDir'
 import json from '@rollup/plugin-json'
 import terser from '@rollup/plugin-terser'
+import { parse } from 'jsonc-parser'
+import { isMatch } from 'micromatch'
 import type { OutputOptions, OutputPlugin, RollupOptions } from 'rollup'
 import esbuild from 'rollup-plugin-esbuild'
+import ts from 'typescript'
 
-import skip from '#~/rollup/plugins/skip.ts'
+import skip from './plugins/skip'
 
 export interface TemplateOptions {
 }
@@ -68,6 +71,83 @@ const withMinify = (
   }
 ]
 
+type TSConfig = {
+  extends?: string | string[]
+  compilerOptions?: Record<string, unknown>
+  references?: { path: string }[]
+  files?: string[]
+  include?: string[]
+  exclude?: string[]
+}
+
+const getTSConfig = (p: string): TSConfig =>
+  !fs.existsSync(p) || !fs.statSync(p).isFile()
+    ? {}
+    : parse(fs.readFileSync(p, 'utf-8'), [], { allowTrailingComma: true, allowEmptyContent: true })
+
+const getExtendTSConfig = (tsconfigPath: string): TSConfig => {
+  tsconfigPath = resolve(tsconfigPath)
+  const tsconfigPathDirname = dirname(tsconfigPath)
+  const { extends: exts, ...tsconfig } = getTSConfig(tsconfigPath)
+  const resolvePaths = (paths: string[] | undefined) => paths?.map(p => resolve(tsconfigPathDirname, p)) ?? []
+
+  const extendsPaths = resolvePaths(
+    exts ? Array.isArray(exts) ? exts : [exts] : []
+  )
+  if (extendsPaths.length === 0) return tsconfig
+  return extendsPaths
+    .map(getExtendTSConfig)
+    // https://www.typescriptlang.org/tsconfig/#files:~:text=Currently%2C%20the%20only%20top%2Dlevel%20property%20that%20is%20excluded%20from%20inheritance%20is%20references.
+    // Currently, the only top-level property that is excluded from inheritance is references.
+    .reduce((acc, { compilerOptions = {}, references: _, ...curr }) => ({
+      ...acc,
+      ...curr,
+      compilerOptions: {
+        ...acc.compilerOptions,
+        ...compilerOptions
+      }
+    }), tsconfig)
+}
+
+const getCompilerOptionsByFilePath = (tsconfigPath: string, filePath: string): Record<string, unknown> | undefined => {
+  tsconfigPath = resolve(tsconfigPath)
+  filePath = resolve(filePath)
+  const tsconfigPathDirname = dirname(tsconfigPath)
+  // https://www.typescriptlang.org/tsconfig/#files:~:text=It%E2%80%99s%20worth%20noting%20that%20files%2C%20include%2C%20and%20exclude%20from%20the%20inheriting%20config%20file%20overwrite%20those%20from%20the%20base%20config%20file%2C%20and%20that%20circularity%20between%20configuration%20files%20is%20not%20allowed.
+  // It’s worth noting that files, include, and exclude from the inheriting config file overwrite
+  // those from the base config file, and that circularity between configuration files is not allowed.
+  const tsconfig = getExtendTSConfig(tsconfigPath)
+  const existReferences = !!tsconfig.references
+  if (!existReferences) return tsconfig.compilerOptions ?? {}
+
+  const resolvePaths = (paths: string[] | undefined) => paths?.map(p => resolve(tsconfigPathDirname, p)) ?? []
+
+  const [
+    references,
+    files,
+    include,
+    exclude
+  ] = [
+    tsconfig.references?.map(({ path }) => path),
+    tsconfig.files,
+    tsconfig.include,
+    tsconfig.exclude
+  ].map(resolvePaths)
+  if (exclude.length > 0 && exclude.some(i => isMatch(filePath, i))) return
+
+  let isInclude = false
+  isInclude ||= files.length > 0 && files.includes(filePath)
+  isInclude ||= include.length > 0 && include.some(i => isMatch(filePath, i))
+  if (isInclude) return tsconfig.compilerOptions ?? {}
+
+  references.reverse()
+  for (const ref of references) {
+    const compilerOptions = getCompilerOptionsByFilePath(ref, filePath)
+    if (compilerOptions) return compilerOptions
+  }
+  return tsconfig.compilerOptions
+}
+
 const generateConfigs = ({
   name,
   input,
@@ -96,6 +176,18 @@ const generateConfigs = ({
       dtsTSConfigPath = p
     }
   })
+  let compilerOptions: ts.CompilerOptions = {}
+  if (dtsTSConfigPath) {
+    const jsonCompilerOptions = getCompilerOptionsByFilePath(dtsTSConfigPath, resolve(input))
+    const { options, errors } = ts.convertCompilerOptionsFromJson(
+      jsonCompilerOptions,
+      dirname(dtsTSConfigPath)
+    )
+    if (errors.length > 0) {
+      throw new Error(errors.map(e => e.messageText).join('\n'))
+    }
+    compilerOptions = options
+  }
   return [
     {
       input,
@@ -134,7 +226,7 @@ const generateConfigs = ({
       ],
       plugins: [
         skip({ patterns: [STYLE_REGEXP] }),
-        dts({ tsconfig: dtsTSConfigPath })
+        dts({ compilerOptions })
       ]
     }
   ]
