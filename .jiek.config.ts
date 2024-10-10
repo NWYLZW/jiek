9c14bb29241829cfe8036b16525937eef192c06a
yijie4188 <yijie4188@gmail.com>
2024-10-10 17:39:25 +0800

    chore: import `defineConfig` to infer type

--- .jiek.config.ts
+++ .jiek.config.ts
@@ -1,8 +1,10 @@
-export default {
+import { defineConfig } from 'jiek'
+
+export default defineConfig({
   init: {
     named: {
       'packages/rollup-plugins/*': 'rollup-plugin-$basename',
       'packages/vite-plugins/*': 'vite-plugin-$basename'
     }
   }
-}
+})
