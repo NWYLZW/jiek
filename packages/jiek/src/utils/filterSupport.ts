c5ac879fbebd3a18fabc74f81da4a98a9fecc3bc
YiJie <yijie4188@gmail.com>
2024-12-28 03:24:03 +0800

    chore(jiek): eslint error

--- packages/jiek/src/utils/filterSupport.ts
+++ packages/jiek/src/utils/filterSupport.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs'
 import path from 'node:path'
+import process from 'node:process'
 
 import { program } from 'commander'
 import { load } from 'js-yaml'
@@ -14,14 +15,16 @@ try {
   type = 'pnpm'
 } catch { /* empty */ }
 
+export interface Manifest {
+  name?: string
+  type?: string
+  exports?: string | string[] | Record<string, unknown>
+}
+
 export interface ProjectsGraph {
   wd: string
   root?: string
-  value?: Record<string, {
-    name?: string
-    type?: string
-    exports?: string | string[] | Record<string, unknown>
-  }>
+  value?: Record<string, Manifest>
 }
 
 export async function filterPackagesGraph(filters: string[]): Promise<ProjectsGraph[]> {
@@ -29,7 +32,7 @@ export async function filterPackagesGraph(filters: string[]): Promise<ProjectsGr
 }
 
 export async function getSelectedProjectsGraph(
-  filter = program.getOptionValue('filter')
+  filter = program.getOptionValue('filter') as string | undefined
 ): Promise<ProjectsGraph> {
   const { wd, notWorkspace } = getWD()
   let root = getRoot()
@@ -38,7 +41,7 @@ export async function getSelectedProjectsGraph(
       wd,
       root,
       value: {
-        [wd]: JSON.parse(fs.readFileSync(path.resolve(wd, 'package.json'), 'utf-8'))
+        [wd]: JSON.parse(fs.readFileSync(path.resolve(wd, 'package.json'), 'utf-8')) as Manifest
       }
     }
   }
@@ -48,20 +51,20 @@ export async function getSelectedProjectsGraph(
     const pnpmWorkspace = load(pnpmWorkspaceFileContent) as {
       packages: string[]
     }
-    if (root === wd && !filter) {
+    if (root === wd && (filter == null)) {
       throw new Error('root path is workspace root, please provide a filter')
       // TODO inquirer prompt support user select packages
     }
     if (root === undefined) {
       root = process.cwd()
     }
-    if (root !== wd && !filter) {
+    if (root !== wd && (filter == null)) {
       const packageJSONIsExist = fs.existsSync(path.resolve(root, 'package.json'))
       if (!packageJSONIsExist) {
         throw new Error('root path is not workspace root, please provide a filter')
       }
-      const packageJSON = JSON.parse(fs.readFileSync(path.resolve(root, 'package.json'), 'utf-8'))
-      if (!packageJSON.name) {
+      const packageJSON = JSON.parse(fs.readFileSync(path.resolve(root, 'package.json'), 'utf-8')) as Manifest
+      if (packageJSON.name == null) {
         throw new Error('root path is not workspace root, please provide a filter')
       }
       filter = packageJSON.name
