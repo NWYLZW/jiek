016302439b27938f6a8eb427efb954e6a32e11f5
YiJie <yijie4188@gmail.com>
2024-12-28 15:23:48 +0800

    chore(jiek): fix eslint error

--- packages/jiek/src/rollup/utils/externalResolver.ts
+++ packages/jiek/src/rollup/utils/externalResolver.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs'
 import { builtinModules } from 'node:module'
+import process from 'node:process'
 
 const EXCLUDE_SUFFIX = [
   'te?xt',
@@ -7,21 +8,28 @@ const EXCLUDE_SUFFIX = [
   '(css|s[ac]ss|less|styl)'
 ]
 
-export default function(json: Record<string, unknown>): (string | RegExp)[]
+interface Manifest {
+  name?: string
+  dependencies?: Record<string, unknown>
+  peerDependencies?: Record<string, unknown>
+  optionalDependencies?: Record<string, unknown>
+}
+
+export default function(json: Manifest): (string | RegExp)[]
 export default function(path?: string): (string | RegExp)[]
-export default function(jsonOrPath: string | Record<string, unknown> = process.cwd()): (string | RegExp)[] {
-  const pkg = typeof jsonOrPath === 'string'
+export default function(jsonOrPath: string | Manifest = process.cwd()): (string | RegExp)[] {
+  const pkg: Manifest = typeof jsonOrPath === 'string'
     ? fs.existsSync(`${jsonOrPath}/package.json`)
-      ? JSON.parse(fs.readFileSync(`${jsonOrPath}/package.json`, 'utf-8'))
-      : {}
+      ? JSON.parse(fs.readFileSync(`${jsonOrPath}/package.json`, 'utf-8')) as Manifest
+      : {} as Manifest
     : jsonOrPath
   const { name, dependencies = {}, peerDependencies = {}, optionalDependencies = {} } = pkg
-  if (!name) {
+  if (name == null) {
     throw new Error('package.json must have a name field')
   }
 
-  const external = <(string | RegExp)[]> Object
-    .keys(dependencies)
+  const external = (<(string | RegExp)[]> [])
+    .concat(Object.keys(dependencies))
     .concat(Object.keys(peerDependencies))
     .concat(Object.keys(optionalDependencies))
     .concat(builtinModules)
