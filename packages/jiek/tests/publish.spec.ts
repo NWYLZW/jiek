6b36d6a4f0e2f897a053d2676df0f046a97af0fa
YiJie <yijie4188@gmail.com>
2025-01-01 00:55:57 +0800

    chore(jiek): update publish snapshot

--- packages/jiek/tests/publish.spec.ts
+++ packages/jiek/tests/publish.spec.ts
@@ -9,12 +9,18 @@ const { describe } = createDescribe({
 describe('no mono', ({ test }) => {
   test('simple', async ({ exec }) => {
     await exec({ cmd: 'build', autoSnapDist: false })
-    await exec({ cmd: 'prepublish' })
-    await exec({ cmd: 'postpublish', autoSnapDist: false })
+    try {
+      await exec({ cmd: 'prepublish' })
+    } finally {
+      await exec({ cmd: 'postpublish', autoSnapDist: false })
+    }
   })
   test('subpath', async ({ exec }) => {
     await exec({ cmd: 'build', autoSnapDist: false })
-    await exec({ cmd: 'prepublish' })
-    await exec({ cmd: 'postpublish', autoSnapDist: false })
+    try {
+      await exec({ cmd: 'prepublish' })
+    } finally {
+      await exec({ cmd: 'postpublish', autoSnapDist: false })
+    }
   }, 10000)
 }, true)
