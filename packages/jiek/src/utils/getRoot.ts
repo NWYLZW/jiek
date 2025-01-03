4362b0c7fb282bb1e67d2dd3b01369c2ee9a8e2d
YiJie <yijie4188@gmail.com>
2025-01-03 22:21:02 +0800

    chore(jiek): eslint error

--- packages/jiek/src/utils/getRoot.ts
+++ packages/jiek/src/utils/getRoot.ts
@@ -1,11 +1,12 @@
 import path from 'node:path'
+import process from 'node:process'
 
 let root: string | undefined
 export function getRoot() {
-  if (root) return root
+  if (root != null) return root
 
   const rootOption = process.env.JIEK_ROOT
-  root = rootOption
+  root = rootOption != null
     ? path.isAbsolute(rootOption)
       ? rootOption
       : path.resolve(process.cwd(), rootOption)
