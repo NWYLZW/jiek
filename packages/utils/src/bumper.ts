7b224a019e1c8f370d843f3a678d7e3372d95dfe
YiJie <yijie4188@gmail.com>
2025-01-04 17:05:37 +0800

    feat(utils): attach tag after version end position

--- packages/utils/src/bumper.ts
+++ packages/utils/src/bumper.ts
@@ -1,6 +1,4 @@
-export type BumperType = 'patch' | 'minor' | 'major' | 'alpha' | 'beta' | 'rc' | 'dev' | (string & {})
-
-export const TAGS = ['alpha', 'beta', 'rc', 'dev']
+export type BumperType = 'patch' | 'minor' | 'major' | (string & {})
 
 export function bump(originalVersion: string, bumper: BumperType) {
   const [version, preRelease] = originalVersion.split('-')
@@ -20,15 +18,9 @@ export function bump(originalVersion: string, bumper: BumperType) {
       return `${major}.${minor + 1}.0`
     case 'major':
       return `${major + 1}.0.0`
-    case 'alpha':
-      return `${major}.${minor}.${patch}-alpha.${preReleaseVersion + 1}`
-    case 'beta':
-      return `${major}.${minor}.${patch}-beta.${preReleaseVersion + 1}`
-    case 'rc':
-      return `${major}.${minor}.${patch}-rc.${preReleaseVersion + 1}`
-    case 'dev':
-      return `${major}.${minor}.${patch}-dev.${preReleaseVersion + 1}`
     default:
-      return bumper
+      return /^\d+\.\d+\.\d+/.test(bumper)
+        ? bumper
+        : `${major}.${minor}.${patch}-${bumper}.${preReleaseVersion + 1}`
   }
 }
