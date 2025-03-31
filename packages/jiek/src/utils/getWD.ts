cdead1cb83bf00373d3e903dc333ede4937fde66
kanno <812137533@qq.com>
2025-03-31 12:07:31 +0800

    feat: remove only pnpm limit (Only build)

--- packages/jiek/src/utils/getWD.ts
+++ packages/jiek/src/utils/getWD.ts
@@ -1,33 +1,70 @@
+import fs from 'fs'
 import process from 'node:process'
 
-import { getWorkspaceDir, isWorkspaceDir } from '@jiek/utils/getWorkspaceDir'
+import path from 'node:path'
+import { searchForWorkspaceRoot } from 'workspace-sieve'
+import type { ProjectManifest } from 'workspace-sieve'
 
-import { type } from './filterSupport'
-import { getRoot } from './getRoot'
+const ROOT_FILES = ['pnpm-workspace.yaml', 'lerna.json']
 
-let wd: string
-let notWorkspace = false
+const LOCK_FILES = ['yarn.lock', 'package-lock.json']
 
-export function getWD() {
-  if (wd) return { wd, notWorkspace }
+export type PackageManagerType = 'pnpm' | 'yarn' | 'npm' | 'lerna' | 'unknown'
 
-  const root = getRoot()
-  if (root !== undefined) {
-    const isWorkspace = isWorkspaceDir(root, type)
-    notWorkspace = !isWorkspace
-    wd = root
-    return { wd, notWorkspace }
+const asserts = {
+  isPnpm: (file: string) => {
+    return file === 'pnpm-workspace.yaml'
+  },
+  isLerna: (file: string) => {
+    return file === 'lerna.json'
+  },
+  isYarn: (file: string) => {
+    return file === 'yarn.lock'
+  },
+  isNpm: (file: string) => {
+    return file === 'package-lock.json'
   }
-  try {
-    wd = getWorkspaceDir(type)
-  } catch (e) {
-    // @ts-ignore
-    if ('message' in e && e.message === 'workspace root not found') {
-      wd = root ?? process.cwd()
-      notWorkspace = true
-    } else {
-      throw e
+}
+
+export interface GetWDResult {
+  wd: string
+  notWorkspace: boolean
+  type: PackageManagerType
+}
+
+export function getWD(): GetWDResult {
+  const root = searchForWorkspaceRoot(process.cwd())
+  for (const file of ROOT_FILES) {
+    if (fs.existsSync(path.join(root, file))) {
+      return {
+        wd: root,
+        notWorkspace: false,
+        type: asserts.isPnpm(file) ? 'pnpm' : asserts.isLerna(file) ? 'lerna' : 'unknown'
+      }
     }
   }
-  return { wd, notWorkspace }
+
+  let lockFile: string = ''
+
+  for (const file of LOCK_FILES) {
+    if (fs.existsSync(path.join(root, file))) {
+      lockFile = file
+      break
+    }
+  }
+
+  try {
+    const packageJSON = JSON.parse(fs.readFileSync(path.join(root, 'package.json'), 'utf-8')) as ProjectManifest
+    return {
+      wd: root,
+      notWorkspace: !!packageJSON.workspaces,
+      type: asserts.isYarn(lockFile) ? 'yarn' : asserts.isNpm(lockFile) ? 'npm' : 'unknown'
+    }
+  } catch {
+    return {
+      wd: root,
+      notWorkspace: true,
+      type: asserts.isYarn(lockFile) ? 'yarn' : asserts.isNpm(lockFile) ? 'npm' : 'unknown'
+    }
+  }
 }
